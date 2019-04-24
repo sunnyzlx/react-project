270b6fc8715dbe2c9001785f21ccc18b00372b74
sunny <sunny@sunnydeMacBook-Pro.local>
2019-04-24 17:14:44 +0800

    引入immutable.js保证store中的state值的不可变性

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -38,7 +38,7 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.header.focused
+    focused: state.header.get('focused')
   }
 }
 
--- src/components/Header/store/reducer.js
+++ src/components/Header/store/reducer.js
@@ -1,18 +1,15 @@
 import * as actionTypes from './actionTypes';
+import { fromJS } from 'immutable';
 
-const defaultState = {
+const defaultState = fromJS({
   focused: false
-}
+})
 
 export default (state = defaultState, action) => {
   if (action.type === actionTypes.SEARCH_FOCUS) {
-    return {
-      focused: true
-    }
+    return state.set('focused', true)
   } else if (action.type === actionTypes.SEARCH_BLUR) {
-    return {
-      focused: false
-    }
+    return state.set('focused', false)
   }
   return state;
 }
